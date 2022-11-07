2508db85b794a436e333de1397c9fc97c2981281
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-07 19:20:08 +0530

    [sourav] add : create contact to address book

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        Contact contact = new Contact();
+
+        public void print()
+        {
+            Console.WriteLine("Contact Derails : " + "\n" + "First Name : " + contact.FirstName + "\n" + "Last Name : " + contact.LastName
+                                + "\n" + "Address is : " + contact.Address + "\n" + "City is : " + contact.City + "\n" + "State is : " + contact.State
+                                + "\n" + "Zip is : " + contact.Zip + "\n" + "Phone Number is :" + contact.PhoneNumber + "\n" + "Email is : " + contact.Email);
+        }
+        public void craeteContact()
+        {
+            contact.FirstName = Console.ReadLine();
+            contact.LastName = Console.ReadLine();
+            contact.Address = Console.ReadLine();
+            contact.City = Console.ReadLine();
+            contact.State = Console.ReadLine();
+            contact.Zip = Convert.ToInt64(Console.ReadLine());
+            contact.PhoneNumber = Console.ReadLine();
+            contact.Email = Console.ReadLine();
+            print();
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,21 +10,8 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Contact contact = new Contact()
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToInt64(Console.ReadLine()),
-                PhoneNumber = Console.ReadLine(),
-                Email = Console.ReadLine()
-            };
-
-            Console.WriteLine("Contact Derails : " + "\n" +"First Name : " + contact.FirstName + "\n" + "Last Name : " + contact.LastName
-                                + "\n" + "Address is : " + contact.Address + "\n" + "City is : " + contact.City + "\n" + "State is : " +  contact.State 
-                                + "\n" + "Zip is : " + contact.Zip + "\n" + "Phone Number is :" + contact.PhoneNumber + "\n" + "Email is : " + contact.Email);
+            AddressBook addressBook = new AddressBook();
+            addressBook.craeteContact();
         }
     }
 }
