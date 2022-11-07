5384786ff33631645d7398917f6776dc99e9e8f0
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-07 17:12:09 +0530

    [sourav] add : welcome message

---
+++ AddressBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System Problem.");
+        }
+    }
+}
