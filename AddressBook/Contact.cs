e4d95c2f4394cda44fca5a38481e1fcb12c74114
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-07 17:42:23 +0530

    [sourav]add : create-Contacts

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address Book System Problem.");
+            Contact contact = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNumber = Console.ReadLine(),
+                Email = Console.ReadLine()
+            };
+
+            Console.WriteLine("Contact Derails : " + "\n" +"First Name : " + contact.FirstName + "\n" + "Last Name : " + contact.LastName
+                                + "\n" + "Address is : " + contact.Address + "\n" + "City is : " + contact.City + "\n" + "State is : " +  contact.State 
+                                + "\n" + "Zip is : " + contact.Zip + "\n" + "Phone Number is :" + contact.PhoneNumber + "\n" + "Email is : " + contact.Email);
         }
     }
 }
